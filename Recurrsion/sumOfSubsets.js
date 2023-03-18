68e34423cb72ff89e29b9ffad00b6e58153797ad
Akash Kumar <86508200+akashsinnghh@users.noreply.github.com>
2023-03-18 15:14:52 +0530

    Create sumOfSubsets.js

---
+++ Recurrsion/sumOfSubsets.js
@@ -0,0 +1,39 @@
+// Given a list (Arr) of N integers, print all the sums of all subsets in it. Output should be printed in increasing order of sums.
+// Input:
+// let N = 3
+// let Arr = [3,1,2]
+// Output: 
+// [0, 1, 2, 3, 3, 4, 5, 6]
+
+// Explaination :
+// When no elements in taken tehn sum = 0
+// when only 2 is taken then sum = 2 
+// when only 3 is taken them sum = 3
+// when element 2 and 3 are taken then sum = 2+3 = 5
+// write code in js and use recurrsion
+
+function printAllSubsets(arr, n, sum = 0, index = 0, output = []) {
+  // Push the current sum to the output array
+  output.push(sum);
+
+  // Recursively call the function for all remaining elements in the array
+  for (let i = index; i < n; i++) {
+    // Include the current element in the sum
+    sum += arr[i];
+
+    // Recursively call the function for the remaining subset
+    printAllSubsets(arr, n, sum, i + 1, output);
+
+    // Exclude the current element from the sum
+    sum -= arr[i];
+  }
+
+  // Return the output array
+  return output;
+}
+
+// Example usage
+const arr = [3,1,2];
+const n = arr.length;
+const output = printAllSubsets(arr, n);
+console.log(output.sort());
