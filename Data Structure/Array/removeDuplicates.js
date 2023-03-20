6f481cf5d8cfe1c776d0fcfd7b1c80c5ca74c72c
Akash Kumar <86508200+akashsinnghh@users.noreply.github.com>
2023-03-20 22:18:29 +0530

    Update removeDuplicates.js

--- Data Structure/Array/removeDuplicates.js
+++ Data Structure/Array/removeDuplicates.js
@@ -5,10 +5,10 @@ function getUnique(arr){
     let uniqueArr = [];
     for(let i of arr) {
         if(uniqueArr.indexOf(i) === -1) {                                    // using indexOf()
-            uniqueArr.push(i);
-        }
-    }
-    console.log(uniqueArr);
+            uniqueArr.push(i);                                            //how does indexOf works?
+        }                                                               //what we are doing here is we have created a empty array. Then when push each index
+    }                                                                   // but indexOf checks that value is present or not in the array, if not it returns `-1`. And
+    console.log(uniqueArr);                                              // the value is pushed into the array. And if present it doesn't returns `-1`
 }
 
 const array = [1, 2, 3, 2, 3];
