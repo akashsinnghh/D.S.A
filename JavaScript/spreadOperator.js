6d543abaa505886215e8c771e5434e015a3ef8d0
Akash Kumar <86508200+akashsinnghh@users.noreply.github.com>
2023-03-24 15:07:21 +0530

    Update spreadOperator.js

--- JavaScript/spreadOperator.js
+++ JavaScript/spreadOperator.js
@@ -5,7 +5,7 @@ function sum(a,b,c){            // rest Parameter " ... "
 }
 
 let arr = [1,2,3]
-sum(...arr)
+// sum(...arr)
 
 //                               Or it can be used to concat()
 
@@ -13,4 +13,17 @@ let arr1 = [1,2,3]
 let arr2 = [4,5,6]
 
 let arr3 = [...arr1,...arr2]
-console.log(arr3)
+// console.log(arr3)
+
+//                               Or it can be used to solve the problem of Call By Reference. How ?
+
+let arr4 = [1,2,3]
+let arr5 = arr4
+// arr5.push(7,8)
+console.log(arr4) //output : [ 1, 2, 3, 7, 8 ]
+console.log(arr5) ////output : [ 1, 2, 3, 7, 8 ]
+
+// To get rid of this we can use spread operator
+
+let arr6 = [...arr4,7,8]
+console.log(arr6)
